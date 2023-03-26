94a245b828cb2efc9abde3f4f377865f5868076c
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2023-03-26 12:01:07 +0300

    Task has done.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using MvcMovie.Models;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Welcome(User user)
+    {
+        ViewData["Message"] = "Hello, " + user.UserName + " !";
+        return View();
+    }
+}
---
+++ MvcMovie/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace MvcMovie.Models
+{
+	public class User
+	{
+		public string UserName { get; set; }
+	}
+}
